703bfba87a9a2b14717f6dce839baa1f0851dba5
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-04 14:07:06 +0200

    Remove unused checkStatus function in API client

--- src/client.js
+++ src/client.js
@@ -36,18 +36,6 @@ const client = {
   },
 };
 
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  } else {
-    const error = new Error(`HTTP Error ${response.statusText}`);
-    error.status = response.statusText;
-    error.response = response;
-    console.log(error);
-    throw error;
-  }
-}
-
 function parseJSON(response) {
   return response.json();
 }
