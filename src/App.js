ba0aee9d6f4383c4308bdec122dc6fe68c414b5c
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-04 16:08:06 +0200

    Tweak app header styles

--- src/App.js
+++ src/App.js
@@ -168,7 +168,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="AppHeader">
-          <h1>Linda from Purcha$ing</h1>
+          <h1 className="AppHeaderHeading">Linda from Purcha$ing</h1>
         </header>
         <ItemList
           items={this.state.items}
