1b71d30e3cbd77c56d6238b3555a2eefc698d5d9
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-11 16:31:07 +0200

    Increment version number to v18

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
   return (
     <header className={styles.Header}>
       <div></div>
-      <h1 className={styles.Heading}>Linda from Purcha$ing v17</h1>
+      <h1 className={styles.Heading}>Linda from Purcha$ing v18</h1>
       <button className={styles.ReloadButton} onClick={props.onReloadClick}>
         <Reload className={styles.ReloadIcon} />
       </button>
