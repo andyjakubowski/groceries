1f119bf34b6c75d3ce477f1710a66cef43958712
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-22 17:26:49 +0200

    Updates

--- customServiceWorker.js
+++ customServiceWorker.js
@@ -18,9 +18,9 @@ const localhostProductionPort = 3010;
 const isLocalhost = Boolean(
   self.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
+    self.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
+    self.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
