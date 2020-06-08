770966dd7e1406da42e7006d97c85a71b490dbbb
Andy Jakubowski <hello@andyjakubowski.com>
2020-06-08 15:19:06 +0200

    Updates

---
+++ precache-manifest.ef19f29a7d6fd72fef05ef662ac69f65.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197927a2b60643f6670500c15866c34b",
+    "url": "/groceries/index.html"
+  },
+  {
+    "revision": "5098703ea72f46433c0a",
+    "url": "/groceries/static/css/main.f973cfa2.chunk.css"
+  },
+  {
+    "revision": "09bd397c04d5fd7bb3cb",
+    "url": "/groceries/static/js/2.b2854f97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/groceries/static/js/2.b2854f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5098703ea72f46433c0a",
+    "url": "/groceries/static/js/main.e045512e.chunk.js"
+  },
+  {
+    "revision": "2c1c7ff98aead2c3e715",
+    "url": "/groceries/static/js/runtime-main.fe436c39.js"
+  },
+  {
+    "revision": "cc96fd33eec1fa068b35b4306b4fece4",
+    "url": "/groceries/static/media/checked.cc96fd33.svg"
+  },
+  {
+    "revision": "1cf2698b76335d29dd0dd087ce81dbe6",
+    "url": "/groceries/static/media/delete.1cf2698b.svg"
+  },
+  {
+    "revision": "c10736c104da62ece12132054b95e588",
+    "url": "/groceries/static/media/reload.c10736c1.svg"
+  },
+  {
+    "revision": "6203d88c3aa3980325f4358196d8ce1f",
+    "url": "/groceries/static/media/unchecked.6203d88c.svg"
+  }
+]);
